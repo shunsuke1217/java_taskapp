962dd4a2deb06c39a50a7bacb103c9897e3d8c5a
shunsuke <mikasyun1217@gmail.com>
2026-09-24 18:56:12 +0900

    Controllerの作成

---
+++ src/main/java/com/example/taskapp/controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.example.taskapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import com.example.taskapp.repository.TaskRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TaskController {
+    
+    private final TaskRepository taskRepository;
+    public TaskController(TaskRepository taskRepository){
+        this.taskRepository=taskRepository;
+    }
+    // /tasksにアクセスしたら全てのタスクを表示する
+    @GetMapping("/tasks")
+    public String list(Model model){
+        // taskRepositoryに保存されているタスクを全て取得してHTMLに渡すためにmodel.tasksに追加する
+        model.addAttribute("tasks", taskRepository.findAll());
+        return "index";
+    }
+
+}
